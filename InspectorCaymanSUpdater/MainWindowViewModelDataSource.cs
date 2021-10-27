58b02b16abbd8aecd92ed35b2aecfd748bd87b0f
Rabkov Evgeniy <rabkov_evgeniy@mail.ru>
2021-10-27 21:39:54 +0700

    Add IMainWindowViewModelDataSource implementation MainWindowViewModelDataSource

---
+++ InspectorCaymanSUpdater/MainWindowViewModelDataSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InspectorCaymanSUpdater
+{
+    class MainWindowViewModelDataSource : IMainWindowViewModelDataSource
+    {
+        public string GetLastDbUpdateDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetLastSoftwereUpdateDate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
