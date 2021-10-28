0e7035f05547a0571caf8a9a74007bf08d2beb2c
Rabkov Evgeniy <rabkov_evgeniy@mail.ru>
2021-10-28 19:34:25 +0700

    Add update loader interface

---
+++ InspectorCaymanSUpdater/UpdateLoaders/IUpdateLoader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InspectorCaymanSUpdater
+{
+    interface IUpdateLoader
+    {
+        Task LoadUpdateAsync(string targetPath);
+    }
+}
