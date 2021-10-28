90f94730d2745e1bdbcd581ff2293a5f34c73383
Rabkov Evgeniy <rabkov_evgeniy@mail.ru>
2021-10-28 22:53:23 +0700

    Add command binding for buttons

--- InspectorCaymanSUpdater/ViewModels/MainWindowViewModel.cs
+++ InspectorCaymanSUpdater/ViewModels/MainWindowViewModel.cs
@@ -5,16 +5,38 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace InspectorCaymanSUpdater
 {
     class MainWindowViewModel: INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        
+        private readonly IUpdateLoader _dbUpdateLoader;
+        private readonly IUpdateLoader _softwereUpdateLoader;
+        private readonly CommonFileDialog _fileDialog;
+        
+        public LoadUpdateCommand LoadDbUpdateCommand 
+        {
+            get 
+            {
+                return _loadDbUpdateCommand ??
+                    (_loadDbUpdateCommand = new LoadUpdateCommand(_dbUpdateLoader, _fileDialog));
+            }
+        }
+        public LoadUpdateCommand LoadSoftwereUpdateCommand
+        {
+            get
+            {
+                return _loadSoftwereUpdateCommand ??
+                    (_loadSoftwereUpdateCommand = new LoadUpdateCommand(_softwereUpdateLoader, _fileDialog));
+            }
+        }
         public string LastSoftwereUpdateDate 
         {
             get => _lastSoftwereUpdateDate;
-            set 
+            set
             {
                 _lastSoftwereUpdateDate = value;
                 OnPropertyChanged("LastSoftwereUpdateDate");
@@ -37,25 +59,28 @@ namespace InspectorCaymanSUpdater
         private string _lastSoftwereUpdateDate;
         private string _lastDbUpdateDate;
         private StringBuilder _logText;
+        private LoadUpdateCommand _loadDbUpdateCommand;
+        private LoadUpdateCommand _loadSoftwereUpdateCommand;
 
-        public  MainWindowViewModel(IMainWindowViewModelDataSource dataSource) 
+        public  MainWindowViewModel(IMainWindowViewModelDataSource dataSource, IUpdateLoader dbUpdateLoader, IUpdateLoader softwereUpdateLoader,
+            CommonFileDialog fileDialog) 
         {
-            if (dataSource == null)
+            if (dataSource == null || dbUpdateLoader == null || softwereUpdateLoader == null)
             {
                 throw new ArgumentNullException();
             }
 
             _logText = new StringBuilder(500);
-            
+            _dbUpdateLoader = dbUpdateLoader;
+            _softwereUpdateLoader = softwereUpdateLoader;
+            _fileDialog = fileDialog;
+
             Task.Run(() =>
             {
                 LogInformation("Извлекаю дату последнего обновления базы данных...");
                 LastDbUpdateDate = dataSource.GetLastDbUpdateDate();
                 LogInformation("Дата последнего обновления базы данных получена.");
-            });
-
-            Task.Run(() =>
-            {
+           
                 LogInformation("Извлекаю дату последнего обновления ПО...");
                 LastSoftwereUpdateDate = dataSource.GetLastSoftwereUpdateDate();
                 LogInformation("Дата последнего обновления ПО получена.");
